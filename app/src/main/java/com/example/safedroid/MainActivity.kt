27bd5a6f06a8b404acf1705cf672942f2f0d0aa9
zhenyasedoy-blip <zhenya_sedoy@inbox.ru>
2026-09-07 03:42:03 +0500

    Initial commit

--- app/src/main/java/com/example/safedroid/MainActivity.kt
+++ app/src/main/java/com/example/safedroid/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val fakeGoogleApiKey = "AIzaSyFakeTestKey1234567890ABCDEFGHIKL"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
